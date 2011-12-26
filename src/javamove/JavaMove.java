7cf3d31f8d84b9a59c090de909491a1bc7fa97bf
jianhuashao <jianhua.shao1986@gmail.com>
2011-12-27 03:44:55 +0800

    init

---
+++ src/javamove/JavaMove.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javamove;
+
+/**
+ *
+ * @author jianhuashao
+ */
+public class JavaMove {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame myframe = new MainFrame();
+        myframe.setVisible(true);
+    }
+}
---
+++ src/javamove/MyThread.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javamove;
+
+/**
+ *
+ * @author jianhuashao
+ */
+public class MyThread extends Thread{
+    
+}
